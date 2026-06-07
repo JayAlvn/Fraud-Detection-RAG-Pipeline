c38c128e5adc1c585070ed659aab30df7146b0da
Testing <test@gmail.com>
2026-06-07 12:55:34 +0200

    test: unit test for generation

--- embedding/vector_store.py
+++ embedding/vector_store.py
@@ -9,14 +9,14 @@ collection = client.get_or_create_collection('fraud_rag')
 def store_chunks(chunks: list[str], doc_name: str):
     
     embeddings = embed_batch(chunks)
-
     ids = [f'{doc_name}_chunk_{i}' for i in range(len(chunks))]
 
     collection.add(documents=chunks, embeddings=embeddings, ids=ids)
 
- #will automatically default to returning the top 5 matches.
+ #default to returning the top 5 matches.
 def query(query_text: str, n=5) -> list[str]:
-    
+
     query_vector = embed_text(query_text)
     results = collection.query(query_embeddings=[query_vector], n_results =n)
+
     return results['documents'][0]
